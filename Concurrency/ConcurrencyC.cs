f6c6764c9c48e69ca5cddc316641f914a6528951
Remus Manta <remus_manta@outlook.com>
2019-06-16 21:31:06 +0100

    Additions to code

--- Concurrency/ConcurrencyC.cs
+++ Concurrency/ConcurrencyC.cs
@@ -1,10 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Concurrency.F;
 
 namespace Concurrency.C
 {
     public static class ConcurrencyC
     {
+        public static IDictionary<string, Person> GetMeADictionaryPlease()
+        {
+            var aDictionary = new Dictionary<string, Person>().ToImmutableDictionary();
+            return aDictionary;
+        }
+
+        
+        /// <summary>
+        /// Parallel for with a shared, mutable variable, 'total' -> define a tlsValue that is used by each thread and aggregate at and in interlocked
+        /// </summary>
+        public static void DoSomething()
+        {
+            long  total = 0;
+            int len = 1000000;
+            Parallel.For(0, len,
+                () => 0,
+                (int i, ParallelLoopState loopState, long tlsValue) =>
+                {
+                    return IsPrime(i) ? tlsValue += i : tlsValue;
+                },
+                value => Interlocked.Add(ref total, value));
+
+        }
+
+        private static bool IsPrime(int i)
+        {
+            return true;
+        }
+    }
+
+
+    //To showcase ImmutableInterlocked.TryAdd works - p.67
+
+    // CAS (compare and swap) mechanism
+    // Implementing atomisation; T needs to immutable. e.g. Immutable List or Dictionary
+    // The compiler, the runtime system, and even hardware may rearrange reads and writes to memory locations for performance reasons. 
+    // Fields that are declared volatile are not subject to these optimizations. 
+    // Adding the volatile modifier ensures that all threads will observe volatile writes performed by any other thread in the order in which they were performed.
+    public sealed class Atom<T> where T : class
+    {
+        public Atom(T value)
+        {
+            _value = value;
+        }
+
+
+        private volatile T _value;
+        public T Value => _value;
+
+        // This is a function that either returns the original T, or un updated, new T with new information (as T is immutable)
+        // This is how ImmutableInterlocked.TryAdd works
+        public T Swap(Func<T, T> factory)
+        {
+            T original, temp;
+            do
+            {
+                original = _value;
+                temp = factory(original);
+            }
+            // if the original is the same as temp, that means no new T was created by another thread, so you can safely return it
+            // exmample when you try to update an Atom<ImmutableDictionary<TKey,Tval>>. p.66
+
+            while (Interlocked.CompareExchange(ref _value, temp, original) != original);
+            return original;
+
+        }
     }
 }
--- ConcurrencyFLib/ConcurrenctFLib1.fs
+++ ConcurrencyFLib/ConcurrenctFLib1.fs
@@ -1,13 +1,99 @@
 namespace Concurrency.F
 
+open System
+open System.Collections.Generic
+open System.Threading.Tasks
+
 type Person (firstName: string, lastName: string) =
     member this.FirstName = firstName
     member this.LastName = lastName
     member this.FullName = String.concat (",") <| [firstName; lastName]
+
+type FList<'a> =
+    | Empty   
+    | Cons of head:'a * tail:FList<'a>   
+
 
 
 module F_Library =
     let LazyF =
         let kibsterFunction = lazy (Person( "Thomas", "McKirbster"))
         kibsterFunction.Force().FullName
-            
+
+    let rec map f (list:FList<'a>) =    
+        match list with
+        | Empty -> Empty
+        | Cons(hd,tl) -> Cons(f hd, map f tl)
+    
+    let rec filter p (list:FList<'a>) =
+        match list with
+        | Empty -> Empty
+        | Cons(hd,tl) when p hd = true -> Cons(hd, filter p tl)
+        | Cons(hd,tl) -> filter p tl
+
+
+
+    // fun and games
+
+    type Suit = 
+        | Hearts 
+        | Clubs 
+        | Diamonds 
+        | Spades
+
+    type Rank = 
+        | Value of int
+        | Ace
+        | King
+        | Queen
+        | Jack
+        static member GetAllRanks() = [ 
+            yield Ace
+            for i in 2 .. 10 do yield Value i
+            yield Jack
+            yield Queen
+            yield King ]
+
+    type SuitRankCombo = {TheSuit: Suit; TheRank:Rank}
+
+    let fullDeck = 
+        [ for suit in [ Hearts; Diamonds; Clubs; Spades] do
+              for rank in Rank.GetAllRanks() do 
+                  yield { TheSuit = suit; TheRank=rank } ]
+
+    let (|DivisibleBy|_|) divideBy n = 
+        if n % divideBy = 0 then Some DivisibleBy else None
+
+    let (|Fizz|Buzz|FizzBuzz|Val|) n = 
+        match n % 3, n % 5 with
+        | 0, 0 -> FizzBuzz
+        | 0, _ -> Fizz
+        | _, 0 -> Buzz
+        | _ -> Val n
+
+    type Operation = Add | Sub | Mul | Div | Pow
+    type Calculator =
+        | Value of double
+        | Expr of Operation * Calculator * Calculator
+
+    type Tree<'a> =    
+        | Empty        
+        | Node of leaf:'a * left:Tree<'a> * right:Tree<'a> 
+
+    let spawn (op:unit->double) = Task.Run(op)
+
+    let rec eval expr =
+        match expr with       
+        | Value(value) -> value       
+        | Expr(op, lExpr, rExpr) ->       
+            let op1 = spawn(fun () -> eval lExpr)   
+            let op2 = spawn(fun () -> eval rExpr)               
+            let apply = Task.WhenAll([op1;op2])   
+            let lRes, rRes = apply.Result.[0], apply.Result.[1]
+            match op with   
+                    | Add -> lRes + rRes
+                    | Sub -> lRes - rRes
+                    | Mul -> lRes * rRes
+                    | Div -> lRes / rRes
+                    | Pow -> System.Math.Pow(lRes, rRes)
+    
